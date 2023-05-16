8ae1429b3d3dd8bfde8c2a65b539beb2ba28205b
rhettb253 <rhettbeardemphl@hotmail.com>
2023-05-16 10:44:30 -0700

    touchup

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// const axios = require('axios');
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3001;
