f98e0b6beb747795b44584a2c8b9c814f9fedb8b
rhettb253 <rhettbeardemphl@hotmail.com>
2023-05-17 19:50:49 -0700

    added cache and timestamps

--- modules/getMovies.js
+++ modules/getMovies.js
@@ -1,13 +1,27 @@
 
 const axios = require('axios');
+const cache = require('./cache');
 
 function getMovies(request, response, next){
   const {city} = request.query;
   const movieDBUrl = `https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
-  axios.get(movieDBUrl)
-    .then(res => res.data.results.map(resObj => new MovieList(resObj)))
-    .then(formattedMovieData => response.status(200).send(formattedMovieData))
-    .catch(err => next(err));
+  const key = city;
+
+  if (cache[key] && Date.now() - cache[key].timestamp < 86400000) {
+    console.log('cache hit- sending data from cache');
+    response.status(200).send(cache[key].data);
+  } else {
+    console.log('cache miss- making request to api');
+    axios.get(movieDBUrl)
+      .then(res => res.data.results.map(resObj => new MovieList(resObj)))
+      .then(formattedMovieData => {
+        cache[key] = {};
+        cache[key].data = formattedMovieData;
+        cache[key].timestamp = Date.now();
+        response.status(200).send(formattedMovieData);
+      })
+      .catch(err => next(err));
+  }
 }
 
 class MovieList {
